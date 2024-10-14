0aaecf93b5f9b03f668f35e79f0b019b517a2b04
AnnaRiccia <ricciardelli.anna99@gmail.com>
2024-10-14 15:28:01 +0000

    Update TestA.py

--- TestA.py
+++ TestA.py
@@ -16,6 +16,9 @@ db = firestore.client()  # Inizializza Firestore
 
 FIREBASE_API_KEY = os.getenv('FIREBASE_API_KEY')
 
+# Inizializza Firestore
+db = firestore.client()
+
 # Funzione per autenticare l'utente con email e password tramite la REST API di Firebase
 def authenticate_user(email, password):
     url = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={FIREBASE_API_KEY}"
@@ -26,6 +29,7 @@ def authenticate_user(email, password):
     }
     response = requests.post(url, json=payload)
     return response.json()
+
 
 # Funzione per inviare una email di recupero password
 def send_password_reset(email):
@@ -40,10 +44,11 @@ def send_password_reset(email):
         return {"error": result['error']['message']}
     return result
 
+
 def app():
     # Finestra laterale
     st.sidebar.title("Menu")
-    
+
     # Pulsante "HOME"
     if st.sidebar.button("HOME"):
         st.session_state.page = "home"
@@ -75,16 +80,16 @@ def app():
                     st.success(f'Benvenuto, {email}!')
                     st.markdown(
                         """
-                            <div style="position: fixed; bottom: 170px; left: 50%; transform: translateX(-50%); background-color: #90EE90; color: black; padding: 10px 20px; border-radius: 5px; text-align: center;">
-                            🎉 Hooray! Good job, but now double click
-                            </div>
-                            """,
-                            unsafe_allow_html=True
+                        <div style="position: fixed; bottom: 170px; left: 50%; transform: translateX(-50%); background-color: #90EE90; color: black; padding: 10px 20px; border-radius: 5px; text-align: center;">
+                        🎉 Hooray! Good job, but now double click
+                        </div>
+                        """,
+                        unsafe_allow_html=True
                     )
                     st.write("<div style='height: 50px;'></div>", unsafe_allow_html=True)
                     st.session_state.page = "user_profile"  # Passa alla pagina del profilo
                 else:
-                    st.warning('Credenziali errate. Riprova.') 
+                    st.warning('Credenziali errate. Riprova.')
 
         # Sezione Sign Up
         elif selezione == 'Sign Up':
@@ -97,7 +102,7 @@ def app():
                     user = auth.create_user(email=email, password=password)
                     st.success('Account creato con successo!')
                     st.markdown(
-                         """
+                        """
                         <div style="position: fixed; bottom: 170px; left: 50%; transform: translateX(-50%); background-color: #90EE90; color: black; padding: 10px 20px; border-radius: 5px; text-align: center;">
                         🎉 Hooray! Good job, but now double click
                         </div>
@@ -127,20 +132,34 @@ def app():
     elif st.session_state.page == "complete_profile":
         st.title('Completa il tuo Profilo')
         st.markdown('### Inserisci le tue informazioni personali')
-
-        first_name = st.text_input('Nome')
         last_name = st.text_input('Cognome')
-        year_of_birth = st.number_input('Anno di nascita', min_value=1900, max_value=2024, step=1)
-
+        first_name = st.text_input('Nome')
+        gender = st.radio('Genere', ['Maschio', 'Femmina'])
+        date_of_birth = st.text_input("Nato il (DD-MM-YYYY)", value="01-01-2000")
+        birthplace = st.text_input('Luogo di Nascita').lower().capitalize()
+        indirizzo = st.text_input('Residenza in via')
+        civico = st.text_input('Civico')
+        comune = st.text_input('Comune')
+        
         if st.button('Salva Informazioni'):
-            # Salva le informazioni nel Firestore
-            db.collection('users').document(st.session_state.user_email).set({
-                'first_name': first_name,
-                'last_name': last_name,
-                'year_of_birth': year_of_birth
-            })
-            st.success('Informazioni salvate con successo!')
-            st.session_state.page = "user_profile"  # Cambia pagina al profilo utente
+            # Verifica che tutti i campi siano compilati e non siano None
+            if None in [first_name, last_name, date_of_birth, birthplace, indirizzo, civico, comune] or \
+                    '' in [first_name, last_name, date_of_birth, birthplace, indirizzo, civico, comune]:
+                st.warning("Tutti i campi devono essere compilati e non possono essere vuoti!")
+            else:
+                # Salva le informazioni nel Firestore come stringa
+                db.collection('users').document(st.session_state.user_email).set({
+                    'first_name': first_name,
+                    'last_name': last_name,
+                    'date_of_birth': date_of_birth,  # Salva come stringa
+                    'gender': gender,
+                    'birthplace': birthplace,
+                    'indirizzo': indirizzo,
+                    'civico': civico,
+                    'comune': comune
+                })
+                st.success('Informazioni salvate con successo!')
+                st.session_state.page = "user_profile"  # Cambia pagina al profilo utente
 
     # Nuova pagina per il profilo utente
     elif st.session_state.page == "user_profile":
@@ -153,18 +172,41 @@ def app():
             user_data = user_doc.to_dict()
             first_name = st.text_input('Nome', value=user_data.get('first_name'), key='first_name')
             last_name = st.text_input('Cognome', value=user_data.get('last_name'), key='last_name')
-            year_of_birth = st.number_input('Anno di nascita', min_value=1900, max_value=2024, step=1, value=user_data.get('year_of_birth'))
+            birthplace = st.text_input('Luogo di Nascita:', value=user_data.get('birthplace'), key='birthplace').lower().capitalize()
+            # Mostra la data di nascita come stringa
+            date_of_birth = user_data.get('date_of_birth') if user_data.get('date_of_birth') else "01-01-2000"
+            date_of_birth_input = st.text_input("Nato il (DD-MM-YYYY)", value=date_of_birth).lower().capitalize()
 
+            opzioni_genere = ['Maschio', 'Femmina']
+            genere_predefinito = user_data.get('gender')
+            index_predefinito = opzioni_genere.index(genere_predefinito) if genere_predefinito in opzioni_genere else 0
+            gender = st.radio('Genere', opzioni_genere, index=index_predefinito, key='gender')
+            indirizzo = st.text_input('Residenza in via', value=user_data.get('indirizzo'), key='indirizzo').lower().capitalize()
+            civico = st.text_input('Civico', value=user_data.get('civico'), key='civico').lower().capitalize()
+            comune = st.text_input('Comune', value=user_data.get('comune'), key='comune').lower().capitalize()
+            
             if st.button('Aggiorna Informazioni'):
-                # Aggiorna i dati nel Firestore
-                db.collection('users').document(st.session_state.user_email).update({
-                    'first_name': first_name,
-                    'last_name': last_name,
-                    'year_of_birth': year_of_birth
-                })
-                st.success('Informazioni aggiornate con successo!')
+                # Verifica che tutti i campi siano compilati e non siano None
+                if None in [first_name, last_name, date_of_birth_input, birthplace, indirizzo, civico, comune] or \
+                        '' in [first_name, last_name, date_of_birth_input, birthplace, indirizzo, civico, comune]:
+                    st.warning("Tutti i campi devono essere compilati e non possono essere vuoti!")
+                else:
+                    # Aggiorna i dati nel Firestore
+                    db.collection('users').document(st.session_state.user_email).update({
+                        'first_name': first_name,
+                        'last_name': last_name,
+                        'gender': gender,
+                        'date_of_birth': date_of_birth_input,
+                        'birthplace': birthplace,
+                        'indirizzo': indirizzo,
+                        'civico': civico,
+                        'comune': comune
+                    })
+                    st.success('Informazioni aggiornate con successo!')
         else:
             st.warning('Nessuna informazione trovata.')  # Messaggio generico
 
+
+# Avvia l'app
 if __name__ == '__main__':
     app()
